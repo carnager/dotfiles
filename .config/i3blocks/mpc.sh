16aa150d48265450bc0d34b4098c04f932f5d36e
Rasmus Steinke <rasi@xssn.at>
2016-01-21 01:01:26 +0100

    dont use intermediate file

--- .config/i3blocks/mpc.sh
+++ .config/i3blocks/mpc.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
-_statusfile=''
-if [[ ! -z ${1} ]]; then
-    _statusfile="${1}"
-fi
-function print_output() {
-    if [[ -z ${_statusfile} ]]; then
-        echo "${1}"
-    else
-        echo "${1}" > "${_statusfile}"
-    fi
-}
+
 while true; do
     playing="$(mpc current -f "[%artist% - ]%title% (%date%)")"
     if [[ -z $playing ]]; then
         playing=" ◼ Stopped"
-        print_output "${playing}"
+        echo "$playing"
     else
         status="►"
         mpc | grep "\[paused\]" 1>/dev/null && status="▷"
         playing="$status $playing"
-        print_output "${playing}"
+        echo "${playing}"
     fi
-    mpc idle player
+    mpc idle player > /dev/null
     sleep 2
 done
 
